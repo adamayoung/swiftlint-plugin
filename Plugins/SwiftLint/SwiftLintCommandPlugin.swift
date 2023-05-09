693eba2e7c8bbdd771fe485230cdf960391ed666
Adam Young <me@adam-young.co.uk>
2023-05-09 21:22:32 +0100

    No cache

--- Plugins/SwiftLint/SwiftLintCommandPlugin.swift
+++ Plugins/SwiftLint/SwiftLintCommandPlugin.swift
@@ -25,9 +25,9 @@ struct SwiftLintCommandPlugin: CommandPlugin {
             swiftlintArguments.append("--strict")
         }
 
-        if argumentExtractor.extractFlag(named: "no-cache") > 0 {
+//        if argumentExtractor.extractFlag(named: "no-cache") > 0 {
             swiftlintArguments.append("--no-cache")
-        }
+//        }
 
         let process = try Process.run(swiftlintExecutableURL, arguments: swiftlintArguments)
         process.waitUntilExit()
